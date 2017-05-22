775d6b8694d0ca5bdb1d451bdf6f8d826c4773a0
diemly0101 <pt.diemly@gmail.com>
2017-05-22 18:15:40 +0700

    upload HTML5 - Local Storage

--- js/main.js
+++ js/main.js
@@ -2,71 +2,89 @@ var $name = document.getElementById('name');
 var $email = document.getElementById('email');
 var $website = document.getElementById('website');
 var $phone = document.getElementById('phone');
-    function validateForm() {
-		checkname()
-		checkmail()
-		checkweb()
-		checkphone()
-	};
 
-	function checkname() {
-		if($name.value == '') {
-			$name.setCustomValidity('Bạn cần phải nhập Tên');
-		}
-		else {
-			$name.setCustomValidity('');  
-		}
-	};
 
-	function checkmail() {
-		if($email.value == '') {
-			$email.setCustomValidity('Bạn cần phải nhập Email');
-		}
-		else if ($email.validity.patternMismatch) {  
-	        $email.setCustomValidity ("bạn cần nhập email đúng định dạng");  
-	    }
-		else {
-			$email.setCustomValidity('');  
-		}
+function validateForm() {
+	checkname();
+	checkmail();
+	checkweb();
+	checkphone();
+};
 
-	};
-
-	function checkweb() {
-
-		if ($website.validity.typeMismatch) {  
-	        $website.setCustomValidity ("Bạn cần phải nhập website đúng định dang");  
-	    }  
-	    else {  
-	       $website.setCustomValidity ("");  
-		} 
-		return false;
-	};
-
-	function checkphone() {
-		if ($phone.validity.patternMismatch) {  
-	        $phone.setCustomValidity ("Bạn cần phải nhập số");  
-	    }  
-	    else {  
-	       $phone.setCustomValidity ("");  
-		} 
-		return false;
-	};
-
-	function checkupload() {
-		var FS = document.getElementById("upload");
-		var files = FS.files;
-
-		// If there is (at least) one file selected
-		if (files.length > 0) {
-		 if (files[0].size > 1024 * 1024) { // Check the constraint
-		   FS.setCustomValidity("Bạn chỉ được Upload file có dụng lượng dưới 1MB");
-		   return;
-		 }
-		}
-		// No custom constraint violation
-		FS.setCustomValidity("");
-	};
-
-	window.onload = function () {
-	  document.getElementById("upload").onchange = checkupload;
+function checkname() {
+	if($name.value == '') {
+		$name.setCustomValidity('Bạn cần phải nhập Tên');
 	}
+	else {
+		$name.setCustomValidity('');  
+	}
+};
+
+function checkmail() {
+	if($email.value == '') {
+		$email.setCustomValidity('Bạn cần phải nhập Email');
+	}
+	else if ($email.validity.patternMismatch) {  
+        $email.setCustomValidity ("bạn cần nhập email đúng định dạng");  
+    }
+	else {
+		$email.setCustomValidity('');  
+	}
+
+};
+
+function checkweb() {
+
+	if ($website.validity.typeMismatch) {  
+        $website.setCustomValidity ("Bạn cần phải nhập website đúng định dang");  
+    }  
+    else {  
+       $website.setCustomValidity ("");  
+	} 
+	return false;
+};
+
+function checkphone() {
+	if ($phone.validity.patternMismatch) {  
+        $phone.setCustomValidity ("Bạn cần phải nhập số");  
+    }  
+    else {  
+       $phone.setCustomValidity ("");  
+	} 
+	return false;
+};
+
+function checkupload() {
+	var FS = document.getElementById("upload");
+	var files = FS.files;
+
+	// If there is (at least) one file selected
+	if (files.length > 0) {
+	 if (files[0].size > 1024 * 1024) { // Check the constraint
+	   FS.setCustomValidity("Bạn chỉ được Upload file có dụng lượng dưới 1MB");
+	   return;
+	 }
+	}
+	// No custom constraint violation
+	FS.setCustomValidity("");
+};
+
+
+window.onload = function () {
+   	document.getElementById("upload").onchange = checkupload;
+
+  	if(localStorage) {
+  		document.getElementById('form').addEventListener('submit', function() {
+  			var $name = document.getElementById('name'),
+	  			$valuname = $name.value;
+	  		localStorage.setItem('name', $valuname);
+	  	});
+  	}
+  	var $getname = localStorage.getItem('name');
+  	if ($getname != "undefined" && $getname != "null" && $getname != "") {
+  		 document.getElementById('message').innerHTML =   "Name: " + $getname
+  	}
+  	 document.getElementById('clear').addEventListener('click', function(){
+            localStorage.removeItem('name');
+    });
+};
